9fb5150ef3bb7c35d95a9758ee7a7c841cebfee2
divyanshgaba <divyansh_gaba@yahoo.co.in>
2017-03-14 12:36:12 +0530

    Week of Code 2, hackerrank

---
+++ Find the Minimum Number/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    for(int i =0;i<n;i++)
+        cout<<"min(int, ";
+    cout<<"int";
+    for(int i =0;i<n;i++)
+        cout<<")";
+    cout<<endl;
+    return 0;
+}
